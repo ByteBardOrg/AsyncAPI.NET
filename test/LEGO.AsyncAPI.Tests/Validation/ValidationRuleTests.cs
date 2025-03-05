7bcc68723b23fec326f15b1c5c0bc1a50c472c35
VisualBean <alekcarlsen@gmail.com>
2025-03-05 10:06:08 +0100

    Remove channel key validation test

--- test/LEGO.AsyncAPI.Tests/Validation/ValidationRuleTests.cs
+++ test/LEGO.AsyncAPI.Tests/Validation/ValidationRuleTests.cs
@@ -10,42 +10,6 @@ namespace LEGO.AsyncAPI.Tests.Validation
     public class ValidationRuleTests
     {
         [Test]
-        [TestCase("chat-{person-id}")]
-        public void V2_ChannelKey_WithInvalidParameter_DiagnosticsError(string channelKey)
-        {
-            var input =
-                $"""
-                asyncapi: 2.6.0
-                info:
-                  title: Chat Application
-                  version: 1.0.0
-                servers:
-                  testing:
-                    url: test.mosquitto.org:1883
-                    protocol: mqtt
-                    description: Test broker
-                channels:
-                  {channelKey}:
-                    publish:
-                      operationId: onMessageReceieved
-                      message:
-                        name: text
-                        payload:
-                          type: string
-                    subscribe:
-                      operationId: sendMessage
-                      message:
-                        name: text
-                        payload:
-                          type: string
-                """;
-
-            var document = new AsyncApiStringReader().Read(input, out var diagnostic);
-            diagnostic.Errors.First().Message.Should().Be($"The key '{channelKey}' in 'channels' MUST match the regular expression '^[a-zA-Z0-9\\.\\-_]+$'.");
-            diagnostic.Errors.First().Pointer.Should().Be("#/channels");
-        }
-
-        [Test]
         public void V2_OperationId_WithNonUniqueKey_DiagnosticsError()
         {
             var input =
