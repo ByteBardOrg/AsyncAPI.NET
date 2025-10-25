7a2ea912e4c1b57cd70b36c1a979790bfd8a32e2
Alex Wichmann <VisualBean@users.noreply.github.com>
2025-10-26 01:13:35 +0200

    Apply suggestion from @VisualBean
    Signed-off-by: Alex Wichmann <VisualBean@users.noreply.github.com>

--- src/ByteBard.AsyncAPI.Readers/Services/DefaultStreamLoader.cs
+++ src/ByteBard.AsyncAPI.Readers/Services/DefaultStreamLoader.cs
@@ -43,7 +43,7 @@
         {
             try
             {
-                if (uri.IsAbsoluteUri && !string.IsNullOrEmpty(uri.Scheme))
+                if (uri.IsAbsoluteUri)
                 {
                     switch (uri.Scheme.ToLowerInvariant())
                     {
